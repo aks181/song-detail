06c58c3a018dbc43c3937b21b5efb74c460a2d19
Akshay <akshaysinghal181@gmail.com>
2020-09-05 22:11:34 +0530

    Updates

---
+++ precache-manifest.9a19b210eb8facc1eab7c9ba219667f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa58ada86699e022e7242d1407a861e",
+    "url": "/song-detail/index.html"
+  },
+  {
+    "revision": "cd59d39378f4eae2399c",
+    "url": "/song-detail/static/js/2.2e14ecbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/song-detail/static/js/2.2e14ecbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb4d38dd184abf44ea6",
+    "url": "/song-detail/static/js/main.78ecddaa.chunk.js"
+  },
+  {
+    "revision": "aae6274e5decb45719e6",
+    "url": "/song-detail/static/js/runtime-main.4c391d46.js"
+  }
+]);
